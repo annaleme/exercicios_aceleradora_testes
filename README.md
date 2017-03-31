3f1716ce2c9dbc98453f0d08592674ac930e2f6b
Leonardo Iglesias <leoalmig@gmail.com>
2017-03-30 23:03:49 -0300

    Add exercises for the first week

---
+++ README.md
@@ -0,0 +1,16 @@
+# Exercicios
+
+## Como rodar os testes?
+
+O código é Ruby. Só precisa executar:  
+
+`ruby exercicio_1/fizzbuzz_test.rb -p`
+`ruby exercicio_2/hamming_test.rb -p`
+
+## Como praticar?
+
+Cada diretório possui 2 arquivos: implementação e teste.  
+Cada teste possui uma linha escrito `skip`.  
+Deve-se remover a linha com `skip` do teste, rodar os testes (e ver ele falhar).
+Então deve-se implementar a funcionalidade (e ver o teste passar). Quando estiver
+pronto, remover o `skip` do próximo teste e assim sucessivamente.
---
+++ exercicio_2/hamming.rb
@@ -0,0 +1,21 @@
+# Calcular a distancia de Hamming entre dois segmentos de DNA
+#
+# Durante a cópia de um ácido nucléico, pode haver uma mutação.
+# A quantidade de nucleotídeos diferentes entre dois segmentos
+# de DNA representa a distância de Hamming.
+#
+# Exemplo:
+# GAGCCTAC
+# GATCCGAC
+#   ^  ^   duas mutações, distância de Hamming é 2
+#
+# A classe Hamming possui uma interface chamada #calcula que
+# recebe duas strings, que são os segmentos a serem comparados
+# Esse método (interface) deve retornar a distância de Hamming
+# entre dois segmentos de DNA.
+
+class Hamming
+  def calcula(segmento_1, segmento_2)
+    # Implementar aqui a solução
+  end
+end
